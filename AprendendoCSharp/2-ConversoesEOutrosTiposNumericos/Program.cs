0d7f13af2a2d449b96edddc6a295aecd42202cbc
Jessica Magalhaes <jfdmagalhaes@hotmail.com>
2020-11-04 18:32:32 -0300

    "adicionando mais algumas informações"

--- AprendendoCSharp/2-ConversoesEOutrosTiposNumericos/Program.cs
+++ AprendendoCSharp/2-ConversoesEOutrosTiposNumericos/Program.cs
@@ -6,22 +6,30 @@ using System.Threading.Tasks;
 
 namespace _2_ConversoesEOutrosTiposNumericos
 {
-   class Program
+    class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Executando projeto 2");
 
-            double salario;
-            salario = 1200.70;
+            double salario = salario = 1200.70;
 
+            //o int é um tipo de variavel que suporta valores ate 32 bits
             int salarioEmInteiro;
             salarioEmInteiro = (int)salario;
 
             Console.WriteLine(salarioEmInteiro);
 
-            int idade;
-            idade = 16;
+            //o short é um tipo de variavel de 64 bits
+            long idade;
+            idade = 13000000000000000;
+
+            //o short é um tipo de variavel de 16 bits
+            short quantidadeProdutos;
+            quantidadeProdutos = 15000;
+
+            float altura = 1.80f;
+
 
             Console.WriteLine("A execução acabou. Tecle enter para sair...");
             Console.ReadLine();
