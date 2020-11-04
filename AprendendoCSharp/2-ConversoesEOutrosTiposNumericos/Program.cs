00d651d35254e9e69acd451a24d46b5587a07430
Jessica Magalhaes <jfdmagalhaes@hotmail.com>
2020-11-04 18:21:35 -0300

    corrigindo nome da pasta

---
+++ AprendendoCSharp/2-ConversoesEOutrosTiposNumericos/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_ConversoesEOutrosTiposNumericos
+{
+   class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 2");
+
+            double salario;
+            salario = 1200.70;
+
+            int salarioEmInteiro;
+            salarioEmInteiro = (int)salario;
+
+            Console.WriteLine(salarioEmInteiro);
+
+            int idade;
+            idade = 16;
+
+            Console.WriteLine("A execução acabou. Tecle enter para sair...");
+            Console.ReadLine();
+
+        }
+    }
+}
