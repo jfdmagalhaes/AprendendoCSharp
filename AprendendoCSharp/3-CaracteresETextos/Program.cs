34ba59373c6c040a9baad39dec85f4df323fc1ad
Jessica Magalhaes <jfdmagalhaes@hotmail.com>
2020-11-05 17:55:53 -0300

    Outras aulas - laco de repetição

---
+++ AprendendoCSharp/3-CaracteresETextos/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_CaracteresETextos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 3");
+
+
+            // character
+            char primeiraLetra = 'a';
+            Console.WriteLine(primeiraLetra);
+
+            primeiraLetra = (char)61;
+            Console.WriteLine(primeiraLetra);
+
+
+            primeiraLetra = (char)(primeiraLetra + 1);
+            Console.WriteLine(primeiraLetra);
+
+        }
+    }
+}
---
+++ AprendendoCSharp/4-CalculaPoupanca/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_CalculaPoupanca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 4");
+
+            double valorInvestido = 1000;
+            int contadorMes = 1;
+
+            while (contadorMes <= 12)
+            { 
+                valorInvestido = valorInvestido + valorInvestido * 0.0036;
+                Console.WriteLine("Após " + contadorMes + " meses, você terá " + valorInvestido);
+                contadorMes += 1;
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
